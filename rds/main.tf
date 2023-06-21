9bd27faa8a0271ce6956e57ec1a26477e930f4d4
Uliana Dotsiak <128092976+ula2023@users.noreply.github.com>
2023-06-20 19:43:59 -0500

    added private subnets

---
+++ rds/main.tf
@@ -0,0 +1,15 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "ula2023"
+    workspaces = {
+      name = "vpc-prod"
+    }
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
